dd0c3aa5224fe990235674e6b9d4df504513c227
0Shraddha <shraddhadongol12345@gmail.com>
2022-12-31 20:23:19 +0545

    final commit

--- home.php
+++ home.php
@@ -15,13 +15,36 @@ if (isset($_SESSION['id']) && isset($_SESSION['firstname'])) {
     <title>HOME</title>
 
     <link rel="stylesheet" type="text/css" href="style.css">
-
+<style>
+     body{
+         margin:0;
+     }
+    .info{
+          background:#38A88F;
+          color: #fff;
+          margin:0px;
+          padding: 10px 50px;
+     }
+    a{
+          background:#38A88F;
+          margin: 10px 0 0 50px;
+          color: #fff;
+          padding: 10px 50px;
+          text-decoration:none;
+          position: absolute;
+    }
+    a:hover{
+          background:#298b76;
+          transition: 0.2s ease;
+    }
+</style>
 </head>
 
 <body>
-
+<div class="info">
      <h1>Hello, <?php echo $_SESSION['firstname']; ?></h1>
-
+     <p>Have a great day!</p>
+     </div>
      <a href="logout.php">Logout</a>
 
 </body>
--- registration.php
+++ registration.php
@@ -17,6 +17,7 @@ if(isset($_POST['create'])){
 		$result = $stmtinsert->execute([$firstname, $lastname, $email, $phonenumber, $password]);
 		if($result){
 			echo 'Successfully saved.';
+			header("location:index.php");
 		}else{
 			echo 'There were erros while saving the data.';
 		}
